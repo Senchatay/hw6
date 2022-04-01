e3dee5f503f61d077f9e42fddee51dfeee677dea
Arseniy <landih73@gmail.com>
2022-04-01 19:16:01 +0300

    p-23

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,29 +1,12 @@
 class LoginsController < ApplicationController
-    skip_before_action :check_aut
+    # skip_before_action :check_aut
   def show
   end
 
   def create
-    if params[:password] == "123"
-      session[:login] = params[:login]
-      session[:balance] = 1000 unless session[:balance]
-      t = Time.now.hour
-      session[:time] = t 
-      case t
-      when (4..11)
-        str="Доброе утро"
-      when (12..16)
-        str = "Добрый день"
-      when (17..22)
-        str="Добрый вечер"
-      else
-        str="Доброй ночи"
-      end
-
-      redirect_to :login, notice: str+", #{session[:login]}"
-    else
-      redirect_to :login, notice: "Неверный пароль"
-    end
+    redirect_to :login, notice: LoginService.new(params, session).call
+  rescue
+    redirect_to login_path, notice: "Неверный пароль"
   end
 
   def destroy
---
+++ app/services/login_service.rb
@@ -0,0 +1,62 @@
+class LoginService
+  attr_reader :params, :session
+
+  def initialize(params, session)
+    @params, @session = params, session
+  end
+
+  def call
+    check_password
+    modify_session
+    message
+  end
+
+  private
+
+  def check_password
+    raise if params[:password] != "123"
+  end
+
+  def modify_session
+    session[:login] = params[:login]
+    session[:balance] = 1000 unless session[:balance]
+    t = Time.now.hour
+    session[:time] = t
+  end
+
+  def message
+    case session[:time]
+    when (4..11)
+      str = "Доброе утро"
+    when (12..16)
+      str = "Добрый день"
+    when (17..22)
+      str = "Добрый вечер"
+    else
+      str = "Доброй ночи"
+    end
+    @notice_message = str + ", #{session[:login]}"
+  end
+
+  #     if params[:password] == "123"
+  #       session[:login] = params[:login]
+  #       session[:balance] = 1000 unless session[:balance]
+  #       t = Time.now.hour
+  #       session[:time] = t
+  #       case t
+  #       when (4..11)
+  #         str="Доброе утро"
+  #       when (12..16)
+  #         str = "Добрый день"
+  #       when (17..22)
+  #         str="Добрый вечер"
+  #       else
+  #         str="Доброй ночи"
+  #       end
+
+  #       redirect_to :login, notice: str+", #{session[:login]}"
+  #     else
+  #       redirect_to :login, notice: "Неверный пароль"
+  #     end
+  #   end
+end
