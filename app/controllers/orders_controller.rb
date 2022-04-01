db0b8346a41d72d0ae5596a89e23c187d40f971e
Arseniy <landih73@gmail.com>
2022-04-01 21:18:26 +0300

    p-25,p-26

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,6 +18,8 @@ class OrdersController < ApplicationController
   # GET /orders or /orders.json
   def index
     @orders = Order.all
+    @users = User.all
+    @users.first
     puts "params: #{params.inspect}"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_090421) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_01_171703) do
   create_table "orders", force: :cascade do |t|
     t.string "name"
     t.integer "status"
     t.integer "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
