d343980d4531f0412e812617a4c771e5f4e4e77e
Arseniy <landih73@gmail.com>
2022-04-03 04:02:16 +0300

    Добавлен основной цикл обработки для пункта 3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,24 @@ class OrdersController < ApplicationController
   before_action :set_order, only: %i[ show edit update destroy ]
   skip_before_action :verify_authenticity_token
 
-  def check cpu, ram, hdd_type, hdd_capacity, os
-
+  def check(cpu, ram, hdd_type, hdd_capacity, os)
+    require "json"
+    require "net/http"
+  
+    uri = URI("http://possible_orders.srv.w55.ru/")
+    res = Net::HTTP.get_response(uri)
+  
+    hash = JSON.parse(res.body)
+    current_vm = { "os" => os, "cpu" => cpu, "ram" => ram, "hdd_type" => hdd_type, "hdd_capacity" => hdd_capacity }
+    
+    x = hash["specs"].find { |virtual_machine| #Если в списке ВМ внешнего сервиса найдется такая ВМ, что
+      virtual_machine.select { |key, value| #Значения полей этой ВМ будут совпадать с значениями таких же полей current_vm
+        value.find { |x|
+          (x == current_vm[key]) || (x.kind_of?(Array) && x[0] == current_vm["hdd_type"] && current_vm[key] > x[1]["from"] && current_vm[key] < x[1]["to"]) #Либо обычные поля хэша, либо hdd_capacity, который в find приходит массивом вида ["sata",{"from"=>20,"to"=>100}]
+        }
+      }.length==5 #Совпасть должны все поля
+    }
+  #   puts x
   end
 
   def first
@@ -17,7 +33,7 @@ class OrdersController < ApplicationController
 
   def calc
     # render plain: "OK"
-    @x=rand(100)
+    @x = rand(100)
   end
 
   # GET /orders or /orders.json
@@ -81,13 +97,14 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:name, :status, :cost)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :status, :cost)
+  end
 end
