8f42c893f6d32588e92b4f623130df3be16a093c
Arseniy <landih73@gmail.com>
2022-04-03 05:39:52 +0300

    Дошел до 8 пункта. Прицепил hw4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,21 +5,45 @@ class OrdersController < ApplicationController
   def check(cpu, ram, hdd_type, hdd_capacity, os)
     require "json"
     require "net/http"
-  
+
     uri = URI("http://possible_orders.srv.w55.ru/")
     res = Net::HTTP.get_response(uri)
-  
+    
     hash = JSON.parse(res.body)
     current_vm = { "os" => os, "cpu" => cpu, "ram" => ram, "hdd_type" => hdd_type, "hdd_capacity" => hdd_capacity }
-    
+
+    finded = false
     x = hash["specs"].find { |virtual_machine| #Если в списке ВМ внешнего сервиса найдется такая ВМ, что
       virtual_machine.select { |key, value| #Значения полей этой ВМ будут совпадать с значениями таких же полей current_vm
         value.find { |x|
           (x == current_vm[key]) || (x.kind_of?(Array) && x[0] == current_vm["hdd_type"] && current_vm[key] > x[1]["from"] && current_vm[key] < x[1]["to"]) #Либо обычные поля хэша, либо hdd_capacity, который в find приходит массивом вида ["sata",{"from"=>20,"to"=>100}]
         }
-      }.length==5 #Совпасть должны все поля
+      }.length == 5 #Совпасть должны все поля
     }
-  #   puts x
+    finded = true if x
+
+    str = "http://0.0.0.0:5678/cost?"
+    current_vm.each { |k, v| str += "#{k}=#{v}&" }
+    uri = URI(str.chop!)
+    cost = Net::HTTP.get_response(uri).body.to_f
+    balance_enough = true if session[:balance] >= cost
+
+    if finded && balance_enough
+      balance_before = session[:balance]
+      session[:balance] -= cost
+
+      render status: 200
+      output = { "result" => true, "total" => cost, "balance" => balance_before, "balance_after_transaction" => session[:balance] }
+
+      render json: output
+      return output.to_json
+    elsif finded
+      error = "Недостаточно средств."
+    elsif balance_enough
+      error = "Не найдено подходящих Виртуальных Машин."
+    end
+    render status: 406
+    return { "result" => false, "error" => error }.to_json
   end
 
   def first
